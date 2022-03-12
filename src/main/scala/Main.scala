3268e506450650fde7fa9391845bdcceaccd027d
tvoklov <33697842+tvoklov@users.noreply.github.com>
2022-03-13 01:05:32 +0400

    small fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import scala.io.Source
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println(Source.fromResource("test.conf").getLines().mkString("\n"))
+    println(Source.fromResource("elm/elm.js").getLines().mkString("\n"))
   }
 
 }
